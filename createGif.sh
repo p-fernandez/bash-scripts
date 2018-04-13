cb713833a65ac7ee2b345b7fe7ff346aec6afa23
Pablo Fernández <pablo.fernandez.otero@gmail.com>
2018-04-13 23:27:31 +0200

    Gif generator using ffmpeg

---
+++ createGif.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+FILE=$1
+FRAME_RATE=$2
+
+FILE_NAME=${FILE%%.*} 
+DEST_FILE="$FILE_NAME.gif"
+
+if [[ -z $FRAME_RATE ]]; then
+  FRAME_RATE=12
+fi
+
+ffmpeg -ss 00:00:00 -i "$OG_FILE" -r $FRAME_RATE -vf scale=400:-1 "$DEST_FILE"
+
